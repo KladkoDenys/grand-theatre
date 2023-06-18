24d4209abc58b6077f533622eacb6612c113d6b0
Kladko Denys <kladko.denys@gmail.com>
2023-06-18 16:26:01 +0300

    add navigation

--- script/script.js
+++ script/script.js
@@ -8,13 +8,35 @@ const openBurgerTrigger = document.querySelector('#burger-menu-open-trigger'),
 	closeBurgerTrigger = document.querySelector('#burger-menu__close-trigger'),
 	burgerMenu = document.querySelector('.burger-menu');
 
+function scrollOff (selector) {
+	document.querySelector(selector).classList.add('overflow-hidden');
+}
+function scrollOn (selector) {
+	document.querySelector(selector).classList.remove('overflow-hidden')
+}
+
 function openCloseBurger (openTrigger,closeTriger,changeClass) {
 
+
+
 	openTrigger.addEventListener('click',function(){
+		// document.querySelector('body').classList.add('overflow-hidden');
+		scrollOff('body');
 		changeClass.style.left = '0';
+		
 	});
 
+	document.querySelectorAll('.burger-menu a').forEach(el => {
+			el.addEventListener('click', function() {
+				scrollOn('body');
+				changeClass.style.left = '-100%';
+			})
+	})
+	
+
 	closeTriger.addEventListener('click', function (){
+		// document.querySelector('body').classList.remove('overflow-hidden')
+		scrollOn('body');
 		changeClass.style.left = '-100%';
 	})
 }
@@ -39,9 +61,11 @@ openCloseBurger(openBurgerTrigger,closeBurgerTrigger,burgerMenu);
 
 		openTrigger.addEventListener('click',function(){
 			changeClass.style.display = 'flex';
+			scrollOff('body');
 		});
 	
 		closeTriger.addEventListener('click', function (){
+			scrollOn('body');
 			changeClass.style.display = 'none';
 		})
 	}
@@ -142,5 +166,75 @@ openCloseBurger(openBurgerTrigger,closeBurgerTrigger,burgerMenu);
 	}
 
 	ourPostersSlides();
+
+//--------------------
+//----BTN -UP
+//--------------------
+
+const btnUp = {
+	el: document.querySelector('.btn-up'),
+	scrolling: false,
+	show() {
+	  if (this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+		 this.el.classList.remove('btn-up_hide');
+		 this.el.classList.add('btn-up_hiding');
+		 window.setTimeout(() => {
+			this.el.classList.remove('btn-up_hiding');
+		 }, 300);
+	  }
+	},
+	hide() {
+	  if (!this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+		 this.el.classList.add('btn-up_hiding');
+		 window.setTimeout(() => {
+			this.el.classList.add('btn-up_hide');
+			this.el.classList.remove('btn-up_hiding');
+		 }, 300);
+	  }
+	},
+	addEventListener() {
+	  // при прокрутке окна (window)
+	  window.addEventListener('scroll', () => {
+		 const scrollY = window.scrollY || document.documentElement.scrollTop;
+		 if (this.scrolling && scrollY > 0) {
+			return;
+		 }
+		 this.scrolling = false;
+		 // если пользователь прокрутил страницу более чем на 200px
+		 if (scrollY > 400) {
+			// сделаем кнопку .btn-up видимой
+			this.show();
+		 } else {
+			// иначе скроем кнопку .btn-up
+			this.hide();
+		 }
+	  });
+	  // при нажатии на кнопку .btn-up
+	  document.querySelector('.btn-up').onclick = () => {
+		 this.scrolling = true;
+		 this.hide();
+		 // переместиться в верхнюю часть страницы
+		 window.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'smooth'
+		 });
+	  }
+	}
+ }
+ 
+ btnUp.addEventListener();
+
+ //--------------------
+//----Input - help
+//--------------------
+
+	const inputTelTrigger = document.querySelector('.patron__tel'),
+			helpWindow = document.querySelector('.patron__tel-help');
+
+	inputTelTrigger.addEventListener('focus', function(){
+		helpWindow.style.display = 'flex';
+	})
+
 
 });
